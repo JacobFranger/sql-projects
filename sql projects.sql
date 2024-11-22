b484939b2911c993e26d18b4cfe2d79c1adc732a
JacobFranger <jacobfranger@gmail.com>
2024-11-22 16:08:01 -0600

    initial commit

---
+++ sql projects.sql
@@ -0,0 +1,16 @@
+Use northwind;
+
+
+CREATE TABLE Dealership (
+AdId int NOT NULL auto_increment,
+ Title varchar(50) NOT NULL, 
+ Addres varchar(50),
+ Phone varchar(12), 
+ PercentOFF float,
+	PRIMARY KEY (AdID)
+);
+
+
+CREATE TABLE vhecilces(
+
+
